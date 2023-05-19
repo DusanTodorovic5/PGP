9202ce5854f2317dfebc888a4e81c983f5b02291
Dusan Todorovic <dtodorovic40@gmail.com>
2023-05-19 14:45:58 +0200

    chore: comments and docstring

--- src/components.py
+++ src/components.py
@@ -2,5 +2,9 @@ from kivymd.uix.screen import MDScreen
 
 # Define custom components that we are going to use from kivy
 class FloatingButton(MDScreen):
+    """
+    A class to represent FloatingButton component. 
+    This class is empty and will be used by Kivy framework
+    """
     pass
 
--- src/main_app.py
+++ src/main_app.py
@@ -6,9 +6,16 @@ from kivy.config import Config
 import config
 
 class MainApp(MDApp):
+    """
+    MainApp screen class 
+    """
     def __init__(self, **kwargs):
+        """Constructor of MainApp. 
+        Loads the icon, flotaing button data and all of the components avaliable
+        """
         super().__init__(**kwargs)
         Config.set('kivy','window_icon',config.get_icon("icon"))
+        # Loading the floating button data, currently RSA and DSA + ElGamal
         self.floating_button_data = {
             "RSA" : [
                 "lock-check",
@@ -29,16 +36,20 @@ class MainApp(MDApp):
             Builder.load_file(kv_component)
 
     def build(self):
+        """Build method inherited from Kivy's MDApp"""
         return Builder.load_file(config.get_screen("main_screen"))
     
     def on_start(self):
+        """Start method inherited from Kivy's MDApp"""
         self.title = "PGP"
         self.icon = 'security'
         self.theme_cls.primary_palette = "DeepPurple"
 
     def rsa_click(self):
+        """Callback for floating button RSA option"""
         print("RSA CLICKED")
 
     def dsa_clicked(self):
+        """Callback for floating button DSA+ElGamal option"""
         print("DSA CLICKED")
 
